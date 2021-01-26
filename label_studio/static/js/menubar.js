04829d53da48ee61a77caec7c7dd632430f7ead2
Nikita Skryabin <nr@fenelon.ru>
2021-01-27 01:21:26 +0300

    Handle animations

--- label_studio/static/js/menubar.js
+++ label_studio/static/js/menubar.js
@@ -32,6 +32,12 @@ const aroundTransition = (element, {init, transition, onStart, beforeTransition,
   setTimeout(() => transition(element), 50);
 }
 
+const callOrReturn = (value, ...args) => value instanceof Function ? value(...args) : value;
+
+const lsGet = (key) => localStorage.getItem(key)
+
+const lsSet = (key, value) => localStorage.setItem(key, value)
+
 /**
  *
  * @param {HTMLElement} element
@@ -46,7 +52,7 @@ const matchesSelector = (element, selector) => {
 /**
  * @param {HTMLElement} menu
  */
-const setMenuState = (menu, {visible} = {}) => {
+const setMenuState = (menu, {visible, animate} = {}) => {
   const classState = {
     beforeAppear: 'menu-dropdown-before-appear',
     appear: 'menu-dropdown-appear',
@@ -56,41 +62,72 @@ const setMenuState = (menu, {visible} = {}) => {
   const classVisible = 'menu-dropdown-visible';
   const currentlyVisible = menu.classList.contains(classVisible);
 
-  if (currentlyVisible === visible) return;
+  if (currentlyVisible === visible) return visible;
 
-  aroundTransition(menu, {
-    transition() {
-      menu.classList.add(visible ? classState.appear : classState.disappear);
-    },
-    beforeTransition() {
-      menu.classList.add(visible ? classState.beforeAppear : classState.beforeDisappear);
-    },
-    afterTransition() {
-      menu.classList.remove(...Object.values(classState));
-      menu.classList[visible ? 'add' : 'remove'](classVisible);
-    }
-  });
+  if (animate !== false) {
+    aroundTransition(menu, {
+      transition() {
+        menu.classList.add(visible ? classState.appear : classState.disappear);
+      },
+      beforeTransition() {
+        menu.classList.add(visible ? classState.beforeAppear : classState.beforeDisappear);
+      },
+      afterTransition() {
+        menu.classList.remove(...Object.values(classState));
+        menu.classList[visible ? 'add' : 'remove'](classVisible);
+      }
+    });
+  } else {
+    menu.classList[visible ? 'add' : 'remove'](classVisible);
+  }
+
+  return !currentlyVisible;
 }
 
-const toggleMenu = (menu) => {
+const toggleMenu = (menu, options = {}) => {
   const classVisible = 'menu-dropdown-visible';
   let currentlyVisible = menu.classList.contains(classVisible);
 
-  setMenuState(menu, { visible: currentlyVisible ? false : true });
+  return setMenuState(menu, {
+    ...(options ?? {}),
+    visible: currentlyVisible ? false : true
+  });
 }
 
-const attachMenu = (triggerSelector, {trigger = 'mousedown', closeOnClickOutside = true} = {}) => {
+const attachMenu = (triggerSelector, {
+  trigger = 'mousedown',
+  closeOnClickOutside = true,
+  onMenuToggle,
+  animate = true,
+  defaultVisible = false,
+} = {}) => {
   /** @type {HTMLElement} */
-  let menuDropdown = null;
+  let menuDropdown = (() => {
+    const triggeringElement = document.querySelector(triggerSelector);
+    const menuSelector = triggeringElement.dataset?.menu;
+
+    return menuSelector
+      ? document.querySelector(menuSelector)
+      : triggeringElement.querySelector('.menu-dropdown');
+  })();
 
   const shoudlCloseOnClickOutside = () => {
-    return closeOnClickOutside instanceof Function
-      ? closeOnClickOutside(menuDropdown)
-      : closeOnClickOutside;
+    return callOrReturn(closeOnClickOutside, menuDropdown);
   }
 
   const clickedOutside = (target) => {
     return menuDropdown && !menuDropdown.contains(target);
+  }
+
+  const shouldAnimate = () => {
+    return callOrReturn(animate, menuDropdown);
+  }
+
+  if (defaultVisible) {
+    setMenuState(menuDropdown, {
+      visible: true,
+      animate: false,
+    })
   }
 
   document.addEventListener(trigger, (e) => {
@@ -98,29 +135,44 @@ const attachMenu = (triggerSelector, {trigger = 'mousedown', closeOnClickOutside
       e.preventDefault();
       e.stopPropagation();
 
-      /** @type {HTMLElement} */
-      const triggeringElement = e.target;
+      const state = toggleMenu(menuDropdown, {
+        animate: shouldAnimate()
+      });
 
-      const menuSelector = triggeringElement.dataset?.menu;
-
-      menuDropdown = menuSelector
-        ? document.querySelector(menuSelector)
-        : triggeringElement.querySelector('.menu-dropdown');
-
-      console.assert(!!menuDropdown, "Menu dropdown is not attached");
-
-      toggleMenu(menuDropdown);
+      onMenuToggle?.(menuDropdown, state);
     } else if (clickedOutside(e.target) && shoudlCloseOnClickOutside()) {
-      setMenuState(menuDropdown, { visible: false });
+      setMenuState(menuDropdown, {
+        visible: false,
+        animate: shouldAnimate()
+      });
     }
   }, {capture: true});
 }
 
+const ls = {
+  get menuVisible() { return lsGet(this._menuVisibility) == 'true' },
+  set menuVisible(value) { return lsSet(this._menuVisibility, value) },
+  get sidebarPinned() { return lsGet(this._sidebarPinned) == 'true' },
+  set sidebarPinned(value) { return lsSet(this._sidebarPinned, value) },
+
+  /**@private */
+  _menuVisibility: 'main-menu-visible',
+  /**@private */
+  _sidebarPinned: 'sidebarPinned',
+}
+
 // Main menu
 attachMenu('.main-menu-trigger', {
+  defaultVisible: ls.menuVisible,
+  animate(menu) {
+    return menu.classList.contains('sidebar-floating')
+  },
   closeOnClickOutside(menu) {
     return menu.classList.contains('sidebar-floating')
   },
+  onMenuToggle(_, visible) {
+    ls.menuVisible = visible;
+  }
 });
 
 // Project menu
@@ -135,16 +187,16 @@ document.addEventListener('click', (e) => {
 
     if (sidebar.classList.contains('sidebar-floating')) {
       sidebar.classList.remove('sidebar-floating');
-      localStorage.setItem('sidebar-floating', false);
+      ls.sidebarPinned = true;
     } else {
       sidebar.classList.add('sidebar-floating');
-      localStorage.setItem('sidebar-floating', true);
+      ls.sidebarPinned = false;
     }
 
     window.dispatchEvent(new Event('resize'));
   }
 });
 
-if (localStorage.getItem('sidebar-floating') === 'false') {
+if (ls.sidebarPinned) {
   document.querySelector('.sidebar').classList.remove('sidebar-floating');
 }
